43244d82165046b7c7aba0ba15581a3f6e3bbaa7
Michael Steinbaugh <mike@steinbaugh.com>
2020-10-09 09:11:48 -0400

    Add new rnaSeqData function

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,5 @@
 #' @importFrom basejump camelCase
 #' @importFrom cgdsr CGDS getCancerStudies getCaseLists getGeneticProfiles
-#' @importFrom goalie assert isString
+#' @importFrom goalie assert isCharacter isString
 #' @importFrom methods is
 NULL
