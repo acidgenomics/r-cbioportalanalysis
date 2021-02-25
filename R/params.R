0a1b1ce9908e403b55ad794c9d00016e88c64084
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-25 14:55:35 -0500

    Rework NAMESPACE

--- R/params.R
+++ R/params.R
@@ -1,3 +1,22 @@
+#' cBioPortalAnalysis
+#'
+#' Toolkit for performing queries on cBioPortal.
+#'
+#' @importMethodsFrom S4Vectors coerce
+#'
+#' @importFrom basejump camelCase
+#' @importFrom cgdsr CGDS getCancerStudies getCaseLists getGeneticProfiles
+#'   getProfileData
+#' @importFrom cli cli_alert cli_alert_warning
+#' @importFrom dplyr arrange_all bind_rows everything select
+#' @importFrom goalie assert isCharacter isString
+#' @importFrom methods as is
+#' @importFrom rlang !!! !! sym syms
+#' @importFrom tibble rownames_to_column
+"_PACKAGE"
+
+
+
 #' @name params
 #' @inherit AcidRoxygen::params return title
 #' @keywords internal
