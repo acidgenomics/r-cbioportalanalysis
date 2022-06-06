9a978af62a1c326cf26cfea88c1265dcc25e3762
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-06 12:25:30 -0400

    Update NAMESPACE

---
+++ R/imports.R
@@ -0,0 +1,22 @@
+## S4 generics and methods =====================================================
+
+#' @importFrom AcidGenerics camelCase makeDimnames makeNames sanitizeNA
+#' @importFrom BiocGenerics do.call lapply rbind sort unique
+#'
+#' @importMethodsFrom pipette sanitizeNA
+#' @importMethodsFrom syntactic camelCase makeDimnames makeNames
+NULL
+
+
+
+## Standard functions ==========================================================
+
+#' @importFrom AcidCLI abort alert alertWarning
+#' @importFrom AcidExperiment makeSummarizedExperiment
+#' @importFrom IRanges CharacterList
+#' @importFrom cgdsr CGDS getCancerStudies getCaseLists getClinicalData
+#'   getGeneticProfiles getProfileData
+#' @importFrom goalie assert hasColnames hasNoDuplicates hasRownames hasRows
+#'   isCharacter isInt isString isSubset
+#' @importFrom methods as is
+NULL
