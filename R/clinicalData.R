dee68dc655ef492a2888b82437d7abf901c42e06
Michael Steinbaugh <mike@steinbaugh.com>
2022-09-15 12:15:33 -0400

    Update clinicalData wrapper

--- R/clinicalData.R
+++ R/clinicalData.R
@@ -1,7 +1,7 @@
 #' Get available clinical data for samples in a case list
 #'
 #' @export
-#' @note Updated 2022-06-06.
+#' @note Updated 2022-09-15.
 #'
 #' @inheritParams params
 #'
@@ -12,35 +12,27 @@
 #' @return `DataFrame`.
 #'
 #' @examples
-#' ## DLBCL TCGA ====
-#' x <- clinicalData(caseList = "dlbc_tcga_pan_can_atlas_2018_rna_seq_v2_mrna")
+#' x <- clinicalData(studyId = "pancan_pcawg_2020")
 #' print(x)
-#'
-#' ## CCLE Broad 2019 ====
-#' x <- clinicalData(caseList = "ccle_broad_2019_sequenced")
-#' print(x)
-clinicalData <- function(caseList) {
-    assert(isString(caseList))
-    x <- tryCatch(
-        expr = {
-            getClinicalData(x = .cgds(), caseList = caseList)
-        },
-        error = function(e) {
-            ## This currently happens for "ccle_broad_2019_all", for example.
-            abort(sprintf(
-                "{.pkg %s} query failure for {.var %s}.",
-                "cgdsr", caseList
-            ))
-        }
+clinicalData <- function(studyId, .api = NULL) {
+    if (is.null(.api)) {
+        .api <- .api()
+    }
+    assert(
+        isString(studyId),
+        is(.api, "cBioPortal")
+    )
+    x <- cBioPortalData::clinicalData(
+        api = .api,
+        studyId = studyId
     )
     assert(
         is.data.frame(x),
-        hasNoDuplicates(rownames(x))
+        hasNoDuplicates(x[["sampleId"]])
     )
-    rownames(x) <- makeNames(rownames(x))
-    colnames(x) <- camelCase(colnames(x), strict = TRUE)
-    x <- sanitizeNA(x)
     x <- as(x, "DataFrame")
+    rownames(x) <- x[["sampleId"]]
+    colnames(x) <- camelCase(colnames(x), strict = TRUE)
     x <- x[sort(rownames(x)), sort(colnames(x))]
     x
 }
